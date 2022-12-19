d3f7ae8bce4978cddd2788bbdedc7ad2eccaae44
bob.song <git@sbhhbs.com>
2022-12-19 23:24:03 +0800

    fix

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,5 @@
 docker buildx build \
-  --platform linux/arm/v7,linux/arm64,linux/amd64 \
+  --platform linux/amd64 \
   --push -t sbhhbs/tuya_finger_robot_homekit:`git rev-parse --short HEAD` -t sbhhbs/tuya_finger_robot_homekit:latest .
 
 
