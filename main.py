e9e5d8ca523541e185b98d7ac5f2b1a7f3dc86dd
bob.song <git@sbhhbs.com>
2024-02-20 22:15:18 +0800

    local ok

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
-import datetime
 import logging
 import os
 import signal
-import threading
-import time
-import pytz
-import coloredlogs
-from tuya_iot import (
-    TuyaOpenAPI,
-    AuthType,
-    TuyaOpenMQ,
-    TuyaDeviceManager,
-    TuyaHomeManager,
-    TuyaDeviceListener,
-    TuyaDevice,
-    TuyaTokenInfo,
-    TUYA_LOGGER
-)
 
+import coloredlogs
+import tinytuya
 from pyhap.accessory import Accessory
 from pyhap.accessory_driver import AccessoryDriver
 from pyhap.const import CATEGORY_SWITCH
+from tuya_iot import (
+    TuyaOpenAPI,
+    AuthType,
+    TUYA_LOGGER
+)
 
 logger = logging.getLogger(__name__)
 
 coloredlogs.install(level='INFO', milliseconds=True)
 
 TUYA_LOGGER.setLevel(logging.DEBUG)
+
+tinytuya.set_debug(True)
 
 # logging.basicConfig(level=logging.DEBUG)
 
@@ -38,6 +31,11 @@ tuya_username = os.getenv("tuya_username")
 tuya_password = os.getenv("tuya_password")
 tuya_countrycode = os.getenv("tuya_countrycode")
 tuya_schema = os.getenv("tuya_schema")
+
+local_gateway_device_id = os.getenv("local_gateway_device_id")
+local_gateway_local_key = os.getenv("local_gateway_local_key")
+local_finger_bot_cid = os.getenv("local_finger_bot_cid")
+local_finger_bot_device_id = os.getenv("local_finger_bot_device_id")
 
 home_port = int(os.getenv("home_port", '51826'))
 persist_folder = os.getenv('persist_folder', '/homekit_status/')
@@ -61,19 +59,45 @@ class FingerRobot(Accessory):
         self.set_info_service(
             manufacturer='TuyaFingerRobot',
             model='Finger',
-            firmware_revision='1.0',
+            firmware_revision='2.0',
             serial_number=tuya_device_id
         )
+        self.local_gateway = None
+        if local_gateway_device_id and local_gateway_local_key:
+            self.local_gateway = tinytuya.Device(
+                dev_id=local_gateway_device_id,
+                address='Auto',  # Or set to 'Auto' to auto-discover IP address
+                local_key=local_gateway_local_key,
+                version=3.4
+            )
 
     def set_switch(self, state):
         logger.info(f"State set: {state}")
         if state:
-            openapi.connect(tuya_username, tuya_password,
-                            country_code=tuya_countrycode,
-                            schema=tuya_schema
-                            )
-            commands = {'commands': [{'code': 'switch', 'value': True}]}
-            openapi.post('/v1.0/iot-03/devices/{}/commands'.format(tuya_device_id), commands)
+            def do_remotely():
+                openapi.connect(tuya_username, tuya_password,
+                                country_code=tuya_countrycode,
+                                schema=tuya_schema
+                                )
+                commands = {'commands': [{'code': 'switch', 'value': True}]}
+                openapi.post('/v1.0/iot-03/devices/{}/commands'.format(tuya_device_id), commands)
+
+            def do_locally():
+                fingerbot = tinytuya.Device(
+                    local_finger_bot_device_id,
+                    cid=local_finger_bot_cid,
+                    parent=self.local_gateway
+                )
+                fingerbot.turn_on(2)
+
+            if self.local_gateway is not None:
+                try:
+                    do_locally()
+                except:
+                    do_remotely()
+            else:
+                do_remotely()
+
         self.char_switch.value = False
         self.char_switch.notify()
 
